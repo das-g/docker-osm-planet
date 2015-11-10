aef6d9addc54c33bc0df5ae71b54dd0d5061edb5
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2015-11-10 11:57:43 +0100

    do the right thing(TM)
    overwrite file if newer on server, resume previous download otherwise
    
    See http://stackoverflow.com/a/32322550/674064OD

--- src/download_planet.sh
+++ src/download_planet.sh
@@ -4,4 +4,4 @@ set -e
 
 mkdir -p $osm_planet_dir
 cd $osm_planet_dir
-wget ${osm_planet_mirror}${osm_planet_path_relative_to_mirror}
+wget --timestamping --continue ${osm_planet_mirror}${osm_planet_path_relative_to_mirror}
